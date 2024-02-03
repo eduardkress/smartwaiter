364ac6aa665872b6765277c42fccac2c407c4628
eduardkress <kress.eduard@web.de>
2024-02-03 16:51:12 +0100

    added formatCents function to EURO, so we dont have to divide always in the code

--- demo/src/utils/currencies.ts
+++ demo/src/utils/currencies.ts
@@ -1,4 +1,14 @@
-export const EURO = new Intl.NumberFormat('de-DE', {
+interface ExtendedNumberFormat extends Intl.NumberFormat {
+  formatCents: (value: number) => string;
+}
+
+const EURO = new Intl.NumberFormat('de-DE', {
   style: 'currency',
   currency: 'EUR',
-});
+}) as ExtendedNumberFormat;
+
+EURO.formatCents = function(value: number) {
+  return this.format(value / 100)
+}
+
+export { EURO };
