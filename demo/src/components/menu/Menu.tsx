d4d20561fb7ed945350a7d1afd2a16cdd1808aaa
eduardkress <kress.eduard@web.de>
2024-02-16 10:32:09 +0100

    added category description & discount

--- demo/src/components/menu/Menu.tsx
+++ demo/src/components/menu/Menu.tsx
@@ -1,10 +1,36 @@
 import MenuItem from '@/components/menu/MenuItem';
 import Hero from '@/components/menu/Hero';
 import { Menu } from '@/types/restaurant2';
+import { Fragment } from 'react';
 
 interface Props {
   menu: Menu;
 }
+
+const getDayOfWeekName = (dayNumber: number) : string => {
+  switch (dayNumber) {
+    case 1: return "Montag";
+    case 2: return "Dienstag";
+    case 3: return "Mittwoch";
+    case 4: return "Donnerstag";
+    case 5: return "Freitag";
+    case 6: return "Samstag";
+    case 7: return "Sonntag";
+    default: return "";
+  }
+};
+
+const getFormattedTimeByNumber = (timeNumber: number) : string => {
+  let hours = Math.floor(timeNumber/100).toString();
+  if (hours.length === 1) {
+    hours = "0" + hours;
+  }
+  let minutes = (timeNumber % 100).toString();
+  if (minutes.length === 1) {
+    minutes = "0" + minutes;
+  }
+  return hours+":"+minutes;
+};
 
 function Menu({ menu }: Props) {
   return (
@@ -13,9 +39,9 @@ function Menu({ menu }: Props) {
         {menu.categories.map((category) => {
           return (
             <div key={category.id} id={'category' + category.id}>
-              <div className='container mx-auto'>
+              <div className='container mx-auto flex flex-col space-y-2 py-4'>
                 {category.imageUrl && (
-                  <div className='container mx-auto max-w-5xl px-0 py-2'>
+                  <div className='container mx-auto max-w-5xl px-0 pb-2'>
                     <Hero
                       imgSrc={category.imageUrl}
                       heroAlt={category.name}
@@ -23,7 +49,32 @@ function Menu({ menu }: Props) {
                     />
                   </div>
                 )}
-                <h2 className='container mx-auto max-w-5xl px-0 py-4 text-2xl font-bold'>{category.name}</h2>
+                <h2 className='container mx-auto max-w-5xl px-0 text-2xl font-bold'>{category.name}</h2>
+                {(category.description && category.description.length > 0) && (
+                  <div className='container mx-auto max-w-5xl px-0 text-sm font-sans'>
+                    {category.description.map((description, index) => index === category.description!.length-1 ? description : <Fragment>{description}<br/></Fragment>)}
+                  </div>
+                )}
+                {(category.discountId && category.discountId.length > 0) && (
+                  <div className='container flex max-w-5xl flex-col justify-between gap-y-3 rounded-lg border border-gray-300 bg-white p-4 shadow'>
+                    {category.discountId.map((value, index) => {
+                      const discount = menu.discounts!.find(val => val.id === value)!;
+                      return (
+                        <div>
+                          <div className="text-base font-bold sm:text-xl">
+                            {discount.name}
+                          </div>
+                          <div className="text-sm">
+                            Gültig von {getDayOfWeekName(discount.daysOfWeek[0])} - {getDayOfWeekName(discount.daysOfWeek[discount.daysOfWeek.length-1])} von {getFormattedTimeByNumber(discount.from)} - {getFormattedTimeByNumber(discount.until)} Uhr.
+                          </div>
+                          <div>
+                            {discount.description}
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )}
               </div>
               <div className='flex flex-col space-y-5'>
                 {category.productIds.map((productId, index) => {
